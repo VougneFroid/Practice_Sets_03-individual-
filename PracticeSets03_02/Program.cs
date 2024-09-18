87da36adbf73ac73851711069c98fb262e8398ab
VougneFroid <vougnefroidalis2005@gmail.com>
2024-09-18 12:22:31 +0800

    Code added

---
+++ PracticeSets03_02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Leap_Year
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to leap year identifier!\n");
+
+            Console.Write("Enter a year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            bool isLeapYear = IsLeapYear(year);
+            if (isLeapYear)
+            {
+                Console.WriteLine($"The year {year} is a leap year.");
+            }
+            else
+            {
+                Console.WriteLine($"The year {year} is not a leap year.");
+            }
+        }
+
+        static bool IsLeapYear(int year)
+        {
+            if (year % 100 == 0 || year % 4 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ PracticeSets03_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Practice_Set_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to triangle area calculator!!\n");
+
+            Console.Write("Enter the 'a' value: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the 'b' value: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the 'c' value: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            double area = Triangle_Angle(a, b, c);
+            Console.WriteLine($"The area of this triangle is {area}");
+        }
+
+        static double Triangle_Angle(double a, double b, double c)
+        {
+            double s = (a + b + c) / 2;
+            double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+            return area;
+        }
+    }
+}
